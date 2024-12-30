2e4b69d3c41f498f53c23536f546c77b0613ba78
りんご飴196 <kazumac41@gmail.com>
2024-12-31 03:25:46 +0900

    ✨  :切り替えをBukkitEventで

--- src/main/kotlin/com/github/ringoame196_s_mcPlugin/ExampleVoiceChatPlugin.kt
+++ src/main/kotlin/com/github/ringoame196_s_mcPlugin/ExampleVoiceChatPlugin.kt
@@ -1,15 +1,9 @@
 package com.github.ringoame196_s_mcPlugin
 
-import de.maxhenkel.voicechat.api.Group
 import de.maxhenkel.voicechat.api.VoicechatApi
 import de.maxhenkel.voicechat.api.VoicechatPlugin
-import de.maxhenkel.voicechat.api.VoicechatServerApi
 import de.maxhenkel.voicechat.api.events.EventRegistration
-import de.maxhenkel.voicechat.api.events.MicrophonePacketEvent
-import net.md_5.bungee.api.ChatMessageType
-import net.md_5.bungee.api.chat.TextComponent
-import org.bukkit.ChatColor
-import org.bukkit.entity.Player
+import de.maxhenkel.voicechat.api.events.PlayerConnectedEvent
 
 class ExampleVoiceChatPlugin : VoicechatPlugin {
     /**
@@ -35,50 +29,11 @@ class ExampleVoiceChatPlugin : VoicechatPlugin {
      */
 
     override fun registerEvents(registration: EventRegistration?) {
-        registration?.registerEvent(MicrophonePacketEvent::class.java, this::onMicrophone)
+        registration?.registerEvent(PlayerConnectedEvent::class.java, this::onPlayerConnected)
     }
 
-    private fun onMicrophone(e: MicrophonePacketEvent) {
-        val bukkitPlayer = e.senderConnection?.player?.player as Player
-        val offhandItem = bukkitPlayer.inventory.itemInOffHand
-        val offhandItemMeta = offhandItem.itemMeta
-        val itemName = Data.ITEM_NAME
-
+    private fun onPlayerConnected(e: PlayerConnectedEvent) {
         val api = e.voicechat
-        val transceiverNumber = offhandItemMeta?.lore?.get(0)?.toString()
-        var transceiverGroup = if (transceiverNumber != null) {
-            acquisitionGroup(api, transceiverNumber)
-        } else {
-            null
-        }
-        if (offhandItemMeta?.displayName != itemName) transceiverGroup = null
-        val connection = api.getConnectionOf(bukkitPlayer.uniqueId) ?: return
-
-        val connectionGroup = connection.group
-
-        if (connectionGroup != transceiverGroup) {
-            val message = "${ChatColor.YELLOW}トランシーバーを切り替えました"
-            connection.group = transceiverGroup
-            bukkitPlayer.spigot().sendMessage(ChatMessageType.ACTION_BAR, *TextComponent.fromLegacyText(message))
-        }
-    }
-
-    private fun acquisitionGroup(api: VoicechatServerApi, number: String): Group {
-        val groupName = "${Data.DEFAULT_GROUP_NAME}_$number"
-        return Data.groups[groupName] ?: createGroup(api, groupName)
-    }
-
-    private fun createGroup(api: VoicechatServerApi, groupName: String): Group {
-        val groupPass = Data.group_password
-
-        val group = api.groupBuilder()
-            .setPersistent(true) // Doesn't remove the group once all players left
-            .setName(groupName) // The name of the group
-            .setPassword(groupPass) // The password of the group
-            .setType(Group.Type.NORMAL) // The type of the group (NORMAL, OPEN, ISOLATED)
-            .build()
-
-        Data.groups[groupName] = group
-        return group
+        if (Data.api == null) Data.api = api
     }
 }
--- src/main/kotlin/com/github/ringoame196_s_mcPlugin/TransceiverManager.kt
+++ src/main/kotlin/com/github/ringoame196_s_mcPlugin/TransceiverManager.kt
@@ -1,5 +1,8 @@
 package com.github.ringoame196_s_mcPlugin
 
+import de.maxhenkel.voicechat.api.Group
+import de.maxhenkel.voicechat.api.VoicechatServerApi
+import org.bukkit.ChatColor
 import org.bukkit.Material
 import org.bukkit.entity.Player
 import org.bukkit.inventory.ItemStack
@@ -8,9 +11,10 @@ class TransceiverManager {
     fun createItem(number: String): ItemStack {
         val item = ItemStack(Material.SLIME_BALL)
         val itemMeta = item.itemMeta
-        itemMeta?.setDisplayName(Data.ITEM_NAME)
+        itemMeta?.setDisplayName(Data.TRANSCEIVER_ITEM_NAME)
         itemMeta?.lore = mutableListOf(number)
         itemMeta?.setCustomModelData(Data.CUSTOM_MODEL_DATA)
+
         item.itemMeta = itemMeta
         return item
     }
@@ -23,5 +27,48 @@ class TransceiverManager {
             if (item == transceiverItem) return true
         }
         return false
+    }
+
+    fun joinGroup(player: Player, number: String) {
+        val api = Data.api
+
+        if (api == null) {
+            val message = "${ChatColor.RED}現在 トランシーバーの準備中です\n時間をあけてもう一度実行してください"
+            player.sendMessage(message)
+            return
+        }
+
+        val transceiverGroup = acquisitionGroup(api, number)
+        val connection = api.getConnectionOf(player.uniqueId) ?: return
+        connection.group = transceiverGroup
+    }
+
+    fun leaveGroup(player: Player) {
+        val api = Data.api ?: return
+        val connection = api.getConnectionOf(player.uniqueId) ?: return
+        connection.group = null
+    }
+
+    private fun acquisitionGroup(api: VoicechatServerApi, number: String): Group {
+        val groupName = "${Data.DEFAULT_GROUP_NAME}_$number"
+        return Data.groups[groupName] ?: createGroup(api, groupName)
+    }
+
+    private fun createGroup(api: VoicechatServerApi, groupName: String): Group {
+        val groupPass = Data.group_password
+
+        val group = api.groupBuilder()
+            .setPersistent(true) // Doesn't remove the group once all players left
+            .setName(groupName) // The name of the group
+            .setPassword(groupPass) // The password of the group
+            .setType(Group.Type.NORMAL) // The type of the group (NORMAL, OPEN, ISOLATED)
+            .build()
+
+        Data.groups[groupName] = group
+        return group
+    }
+
+    fun acquisitionGroupNumber(item: ItemStack): String? {
+        return item.itemMeta?.lore?.get(0)?.toString()
     }
 }
