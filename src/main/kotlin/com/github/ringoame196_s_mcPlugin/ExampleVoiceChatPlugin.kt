902d2bab6bacc21f0c2a06838c20da1d292da848
りんご飴196 <kazumac41@gmail.com>
2025-01-09 21:30:58 +0900

    ✨  :グループ機能使わずトランシーバー

--- src/main/kotlin/com/github/ringoame196_s_mcPlugin/ExampleVoiceChatPlugin.kt
+++ src/main/kotlin/com/github/ringoame196_s_mcPlugin/ExampleVoiceChatPlugin.kt
@@ -3,7 +3,12 @@ package com.github.ringoame196_s_mcPlugin
 import de.maxhenkel.voicechat.api.VoicechatApi
 import de.maxhenkel.voicechat.api.VoicechatPlugin
 import de.maxhenkel.voicechat.api.events.EventRegistration
-import de.maxhenkel.voicechat.api.events.PlayerConnectedEvent
+import de.maxhenkel.voicechat.api.events.MicrophonePacketEvent
+import net.md_5.bungee.api.ChatMessageType
+import net.md_5.bungee.api.chat.TextComponent
+import org.bukkit.Bukkit
+import org.bukkit.ChatColor
+import org.bukkit.entity.Player
 
 class ExampleVoiceChatPlugin : VoicechatPlugin {
     /**
@@ -29,11 +34,32 @@ class ExampleVoiceChatPlugin : VoicechatPlugin {
      */
 
     override fun registerEvents(registration: EventRegistration?) {
-        registration?.registerEvent(PlayerConnectedEvent::class.java, this::onPlayerConnected)
+        registration?.registerEvent(MicrophonePacketEvent::class.java, this::onMicrophonePacket)
     }
 
-    private fun onPlayerConnected(e: PlayerConnectedEvent) {
-        val api = e.voicechat
-        if (Data.api == null) Data.api = api
+    private fun onMicrophonePacket(e: MicrophonePacketEvent) {
+        val transceiverManager = TransceiverManager()
+        e.senderConnection ?: return
+        val api = e.voicechat ?: return
+        val player = e.senderConnection?.player?.player as? Player ?: return
+        val offHandItem = player.inventory.itemInOffHand.clone()
+        offHandItem.amount = 1
+        transceiverManager.acquisitionTransceiverNumber(offHandItem) ?: return
+        val listenMessage = "${ChatColor.GOLD}[トランシーバー] ${player.name}が発言中"
+        e.cancel()
+
+        var c = 0
+        for (onlinePlayer in Bukkit.getOnlinePlayers()) {
+            if (onlinePlayer == player) continue
+            val onlinePlayerOffHandItem = onlinePlayer.inventory.itemInOffHand.clone()
+            onlinePlayerOffHandItem.amount = 1
+            if (offHandItem != onlinePlayerOffHandItem) continue
+            val onlineConnection = api.getConnectionOf(onlinePlayer.uniqueId) ?: continue
+            api.sendStaticSoundPacketTo(onlineConnection, e.packet.toStaticSoundPacket())
+            onlinePlayer.spigot().sendMessage(ChatMessageType.ACTION_BAR, *TextComponent.fromLegacyText(listenMessage))
+            c ++
+        }
+        val speakerMessage = "${ChatColor.YELLOW}[トランシーバー] ${c}人が聞いています"
+        player.spigot().sendMessage(ChatMessageType.ACTION_BAR, *TextComponent.fromLegacyText(speakerMessage))
     }
 }
